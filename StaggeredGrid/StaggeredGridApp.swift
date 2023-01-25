7864190a748b64e26da8787295e1d6ce9c885471
alaxhenry0121 <alaxhenry0121@gmail.com>
2023-01-25 15:14:35 +0900

    Initial Commit

---
+++ StaggeredGrid/StaggeredGridApp.swift
@@ -0,0 +1,17 @@
+//
+//  StaggeredGridApp.swift
+//  StaggeredGrid
+//
+//  Created by Seungchul Ha on 2023/01/25.
+//
+
+import SwiftUI
+
+@main
+struct StaggeredGridApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
